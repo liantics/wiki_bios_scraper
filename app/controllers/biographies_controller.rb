8ba63eff33c3b85c42271aeaa45590bc7d6d5e34
Liane Allen <liantics.antics@gmail.com>
2014-10-04 21:16:14 -0400

    Recursively collect all the 'next' pages

--- app/controllers/biographies_controller.rb
+++ app/controllers/biographies_controller.rb
@@ -9,12 +9,39 @@ class BiographiesController < ApplicationController
     @pages = doc.xpath("//a[contains(@href,'/wiki/Talk:')]/text()")
     @names = generate_names(@pages)
     @all_category_qualities = doc.xpath("//a[contains(@title,'Category:')]")
+
     next_url = doc.at_xpath("//a[contains(@href,'pagefrom')]/@href")
-    @next_pages = "http://en.wikipedia.org/#{next_url}"
+    @next_page = "http://en.wikipedia.org#{next_url}"
+
+    @num_pages = 5
+    @iterations = 1
+    @page_array = Array.new()
+    @page_array << @next_page
+    traverse_pages(@next_page, @num_pages, @iterations)
+    11.times do
+      @page_array.pop
+    end
+
+    #@num_pages_text = doc.at_xpath("//a[contains(@href,'pagefrom')]/text()")
+    #@old_num_pages = generate_num_pages(@num_pages_text)
   end
 
   def show
+  end
 
+  def traverse_pages(start_page, num_pages, iterations)
+
+    while iterations < num_pages
+      traversal_doc = Nokogiri::HTML(open(start_page))
+
+      next_url = traversal_doc.at_xpath("//a[contains(@href,'pagefrom')]/@href")
+      traversal_next_page = "http://en.wikipedia.org/#{next_url}"
+
+      @page_array << traversal_next_page
+
+      iterations += 1
+      traverse_pages(traversal_next_page, num_pages, iterations)
+    end
   end
 
   private
@@ -28,5 +55,9 @@ class BiographiesController < ApplicationController
     end
 
     cleaned_names
+  end
+
+  def generate_num_pages(page_count)
+    page_count.text.gsub("next ","").to_i
   end
 end
