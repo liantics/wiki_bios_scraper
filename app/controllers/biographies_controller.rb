6891b7dbca18335bbed507ea02b68c9af4c8c957
Liane Allen <liantics.antics@gmail.com>
2014-10-07 13:23:26 -0400

    Refactor biographies controller for readability

--- app/controllers/biographies_controller.rb
+++ app/controllers/biographies_controller.rb
@@ -14,7 +14,11 @@ class BiographiesController < ApplicationController
     @pages_in_this_quality_class = [START_PAGES.second]
     @biography_pages = []
 
-    PageWalker.new.generate_biography_page_list(START_PAGES.second, @biography_pages, @pages_in_this_quality_class)
+    PageWalker.new.generate_biography_page_list(
+      START_PAGES.second,
+      @biography_pages,
+      @pages_in_this_quality_class
+    )
 
     @names = generate_names(@biography_pages.flatten)
   end
@@ -25,13 +29,6 @@ class BiographiesController < ApplicationController
   private
 
   def generate_names(name_list)
-    cleaned_names = []
-
-    name_list.each do |name|
-      stripped_name = name.gsub("Talk:","")
-      cleaned_names << stripped_name
-    end
-
-    cleaned_names
+      name_list.map { |name| name.gsub("Talk:", "") }
   end
 end
